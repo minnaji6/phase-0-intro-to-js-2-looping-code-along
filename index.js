a26887368446987df93294e541109a5dce0329b2
Tosie <minnaji@outlook.com>
2022-03-23 06:52:43 -0400

    commit tree 4e2fb8b176dd8cd2db585f4dee6e5d09a22e0b12

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+// writeCards
+
+function writeCards(names,event) {
+    let thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards;
+}
+
+// countDown
+
+function countDown(initalNumber) {
+   
+    while (0 < initalNumber){
+        console.log(initalNumber)
+        initalNumber -=1
+    }
+    console.log (initalNumber);
+}
